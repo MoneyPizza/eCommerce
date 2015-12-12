c4a9655f4cfa6a06625fd3fb1bfc5f86cf6129e4
MoneyPizza <MoneyPizza@users.noreply.github.com>
2015-12-12 10:36:50 +0100

    Update register.php

--- register.php
+++ register.php
@@ -5,11 +5,12 @@
       $cognome = $_GET['cog'];
       $dataN = $_GET['dn'];
       $cod = rand(1,65535);
-      $query="INSERT INTO Utenti(UTE_Mail,UTE_Password,UTE_Nome,UTE_Cognome,UTE_DataNascita) VALUES ('".$mail."','".$pass."','".$nome."','".$cognome."','".$dataN."');";
+      $query="INSERT INTO Utenti(UTE_Mail,UTE_Password,UTE_Nome,UTE_Cognome,UTE_DataNascita,UTE_Cod) VALUES ('".$mail."','".$pass."','".$nome."','".$cognome."','".$dataN."','".$cod."');";
       $host="10.1.101.40";
-      $mysqli = mysqli_connect($host,"Pippo","boobslean","eCommerce") or die ("errore di connessione");
-      $result = mysqli_query($mysqli,$query) or die("0");
+	$mysqli = mysqli_connect($host,"Pippo","boobslean","eCommerce") or die ("errore di connessione");
+	$result = mysqli_query($mysqli,$query) or die("0");
       echo $result;
       mysqli_close($mysqli);
-      mail($mail,"Account Activation","Please click on the following link to activate your account: ");
+      mail($mail,"Account Activation","Please click on the following link to activate your account:\nhttp://10.1.101.40/accept.php?mail=".$mail."&cod=".$cod);
 ?>
+
